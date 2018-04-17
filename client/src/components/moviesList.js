ce3e1ee5e16255c58d451d29d2db84b84cbd47db
srohimah <sitirohimach@gmail.com>
2018-04-17 13:27:53 +0700

    fetch data to client

--- client/src/components/moviesList.js
+++ client/src/components/moviesList.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import {Query} from 'react-apollo'
-
+import gql from 'graphql-tag'
 
 export default class MoviesList extends Component {
   render() {
     return (
-
       <div>
-        
+        <Query query={gql`
+        {
+          movies{
+            title
+            poster_path
+            overview
+          }
+        }
+        `}
+        >
+        {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+
+        return data.movies.map((movie, index) => (
+          <div key={`movie${index}`}>
+            <p>{movie.overview}</p>
+          </div>
+        ));
+      }}
+        </Query>
       </div>
     )
   }
